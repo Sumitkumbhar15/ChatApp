12e069de21b8ba8197dfc7c80bd4618d37fc3cfe
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-09-18 19:30:37 +0530

    edit route

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ app.post("/chats" , (req,res) => {
     res.redirect("/chats");
 })
 
+// Edit route
+
+app.get("/chats/:id/edit",async (req , res) => {
+    let { id } = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs" , {chat});
+})
+
 app.get("/" , (req , res) => {
     res.send("You are on home page");
 })
