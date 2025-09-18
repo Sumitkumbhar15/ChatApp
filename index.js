1a2938cf8ceaecbac89a56f14a0213a4452a5879
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-09-18 17:04:53 +0530

    index route

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ const Chat = require("./models/chat.js");
 
 app.set("views" , path.join(__dirname , "views"));
 app.set("view engine" , "ejs");
+app.use(express.static(path.join(__dirname , "public")));
+
+main().then(() => {
+    console.log("connection established");
+}).catch((err) => {
+    console.log(err);
+})
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/ChatApp');
+}
+
+// Index route
+
+app.get("/chats" , async (req , res) => {
+    let chats = await Chat.find();
+    console.log(chats);
+    res.render("index.ejs",{chats});
+})
 
 app.get("/" , (req , res) => {
     res.send("You are on home page");
