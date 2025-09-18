ce553449de598071d8e10a09f1942ba1415a7aaa
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-09-18 19:46:48 +0530

    update route

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const mongoose = require("mongoose");
 const path = require("path");
 const port = 8080 ;
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views" , path.join(__dirname , "views"));
 app.set("view engine" , "ejs");
 app.use(express.static(path.join(__dirname , "public")));
 app.use(express.urlencoded({ extended : true}));
+app.use(methodOverride("_method"));
 
 main().then(() => {
     console.log("connection established");
@@ -59,6 +61,22 @@ app.get("/chats/:id/edit",async (req , res) => {
     let { id } = req.params;
     let chat = await Chat.findById(id);
     res.render("edit.ejs" , {chat});
+})
+
+// Update route
+
+app.put("/chats/:id" ,async (req , res) => {
+     let { id } = req.params;
+     let { msg : newMsg} = req.body ;
+     let updatedChat = await Chat.findByIdAndUpdate(id , 
+        {
+            msg : newMsg
+        },
+        {
+            runValidators: true , new : true
+        }
+     );
+     res.redirect("/chats");
 })
 
 app.get("/" , (req , res) => {
