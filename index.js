a39af2733501e3f38e3549e7a91f5c5101be2191
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-09-18 14:11:45 +0530

    basic setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const port = 8080 ;
+
+app.set("views" , path.join(__dirname , "views"));
+app.set("view engine" , "ejs");
+
+main().then((res) => {
+    console.log("connection sucessful");
+})
+.catch ((err) => {
+    console.log(err)
+});
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/ChatApp');
+}
+
+app.get("/" , (req , res) => {
+    res.send("You are on home page");
+})
+
+app.listen(port , () => {
+    console.log(`App listening to port number ${port}`);
+})
